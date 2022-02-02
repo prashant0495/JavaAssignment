f357fcb5fb9967ff537303254b4c7dff67896cd1
prashant0495 <prashantjej1995@gmail.com>
2022-02-02 15:35:39 +0530

    Practice Problem (PP) - Line Comparison Problem

---
+++ comparison.java
@@ -0,0 +1,14 @@
+package com.company;
+//import java.util.Scanner;
+public class comparison {
+    public static void main(String[] args) {
+        //Scanner sc = new Scanner(System.in);
+       // System.out.println("Enter the integer Value to be compare with another one");
+       // Integer integer1 = sc.nextInt();
+        //sc.close();
+        Integer integer1 =new Integer(100);
+        Integer integer2 = new Integer(200)
+                ;
+        System.out.println("compare output: " + integer1.compareTo(integer2));
+    }
+}
